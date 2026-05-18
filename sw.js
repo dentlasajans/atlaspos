8e3f80c77b59b9fbd2f2146fcd17c2d6b6ea3a00
dentlasajans <dentlasajans@users.noreply.github.com>
2026-05-18 07:22:32 +0000

    deploy: 214dcf8231ac8c493a092cac69eb472120a920dc

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'atlaspos-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match('/index.html'))
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
